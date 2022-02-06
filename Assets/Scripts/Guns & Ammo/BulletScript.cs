7f211ff654ae16a25db7d3ec973dfdd84cd96e73
Maxim Kotlyarenko <s1mple.eq.easy@gmail.com>
2022-02-06 13:41:06 +0300

    Fixed bullets physics

--- Assets/Scripts/Guns & Ammo/BulletScript.cs
+++ Assets/Scripts/Guns & Ammo/BulletScript.cs
@@ -8,13 +8,15 @@ public class BulletScript : MonoBehaviour
    
    void OnCollisionEnter2D(Collision2D collision)
    {
+      if(collision.gameObject.tag != "Bullet")
+      {
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);//местоположение возникновения эффекта
         Destroy(effect, 0.1f);//уничтожаем эффект после данного количества времени
         Destroy(gameObject);
+      }
    }
    void Update()
    {
-      if(gameObject.tag != "Bullet")
       Destroy(gameObject, 2.0f); //предполагается удаление объекта если он не попал в цель
    }
 }
